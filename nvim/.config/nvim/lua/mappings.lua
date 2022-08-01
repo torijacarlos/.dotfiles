f043dbcf1e0e8f5ebfd863902fa269d29d18d5d6
torijacarlos <hi@torijacarlos.com>
2022-08-01 13:31:17 -0500

    feature: autocomplete paired symbols

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -6,5 +6,18 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- Buffers
+map("n", "<leader>[", "<cmd>bp<cr>")
+map("n", "<leader>]", "<cmd>bn<cr>")
+
+-- Telescope
 map("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<cr>")
 map("n", "<leader>fg", ":lua require('telescope.builtin').live_grep()<cr>")
+
+-- Autocompletes
+map("i", "(", "()<esc>i")
+map("i", "{", "{}<esc>i")
+map("i", "[", "[]<esc>i")
+map("i", "<", "<><esc>i")
+map("i", "'", "''<esc>i")
+map("i", "\"", "\"\"<esc>i")
