c73f4d46ce327fda551f041fa297cf9578d246d1
torijacarlos <alejandro.torija.b@gmail.com>
2023-04-16 19:08:48 -0600

    feature: added quickfix to telescope mappins

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -7,6 +7,7 @@ vim.keymap.set("n", "<leader>]", "<cmd>bn<cr>", bufopts)
 -- Telescope
 vim.keymap.set("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<cr>", bufopts)
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope.builtin').live_grep()<cr>", bufopts)
+vim.keymap.set("n", "<leader>fq", ":lua require('telescope.builtin').quickfix()<cr>", bufopts)
 
 -- Autocompletes
 vim.keymap.set("i", "(", "()<esc>i", bufopts)
