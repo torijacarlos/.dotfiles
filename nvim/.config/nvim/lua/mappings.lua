9c52ef2ec3dd0098c17c6d8e19347cd9336474ed
torijacarlos <alejandro.torija.b@gmail.com>
2023-04-16 19:08:47 -0600

    feature: tmux conf and telescope settings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -6,4 +6,4 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-map('n', '<leader>ff', ':lua require("telescope.builtin").find_files()<cr>')
+map("n", "<leader>ff", ":lua require('telescope.builtin').find_files()<cr>")
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -53,6 +53,17 @@ packer.startup(function()
     }
 end)
 
+require("telescope").setup({
+    defaults = { 
+        file_ignore_patterns = {'node_modules', '.git'}
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        }
+    }
+})
+
 require("nightfox").setup({
     options = {
         transparent = false
