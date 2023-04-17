66231baeaa249dbf1adf933ab750724c5bcff040
torijacarlos <alejandro.torija.b@gmail.com>
2023-04-16 19:08:48 -0600

    refactor: remove deprecated buf function

--- nvim/.config/nvim/after/plugin/rusttools.lua
+++ nvim/.config/nvim/after/plugin/rusttools.lua
@@ -19,7 +19,7 @@ rt.setup({
 
       vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
       vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-      vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
+      vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
     end,
   },
 })
