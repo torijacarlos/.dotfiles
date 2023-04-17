997b709298a6518b4bc57c9a822f0d63119817f7
torijacarlos <alejandro.torija.b@gmail.com>
2023-04-16 19:08:47 -0600

    feature: add nvim conf

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,25 @@
+vim.go.showmatch = true
+vim.go.wildmode = 'lastused,longest,list'
+vim.opt.colorcolumn = "120"
+vim.opt.signcolumn = "yes"
+
+vim.o.expandtab = true
+vim.o.hlsearch = true
+vim.o.ignorecase = true
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+vim.o.syntax = 'on'
+vim.o.tabstop = 4
+vim.o.scrolloff = 8
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+vim.bo.swapfile = false
+vim.bo.wrapmargin = 100
+
+
+require('mappings')
+require('plugins')
---
+++ nvim/.config/nvim/lua/mappings.lua
@@ -0,0 +1,9 @@
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('n', '<leader>ff', ':lua require("telescope.builtin").find_files()<cr>')
